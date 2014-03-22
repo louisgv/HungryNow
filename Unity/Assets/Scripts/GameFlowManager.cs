db2294aa660e9c8e46f08f5cd936663ef8c7ca44
Louis Graham Vichy <lgvichy@gmail.com>
2014-03-22 06:24:16 -0700

    First Scene
    Title Scene

---
+++ Unity/Assets/Scripts/GameFlowManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameFlowManager : MonoBehaviour {
+
+	public int countOffset = 2;
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	//float timer = 3;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(CountSeconds());
+	}
+
+	IEnumerator CountSeconds(){
+		int seconds = 0;
+		while (true){
+			for (float timer = 0; timer < 1; timer += Time.deltaTime)
+				yield return 0;
+			seconds++;
+			if ((seconds%countOffset)==0)
+				Debug.Log(seconds +" seconds have passed.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (Time.realtimeSinceStartup);
+	
+	}
+
+}
---
+++ Unity/Assets/Scripts/MainTitle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainTitle : MonoBehaviour {
+
+	private bool clicked = false;
+	public float timer = 2;
+	void Update(){
+		if (clicked){
+			timer -= Time.deltaTime;
+			if (timer <= 0){
+				Application.LoadLevel("BaconShooter");
+				Debug.Log("Loaded!");
+			}
+
+		}
+	}
+
+	void OnMouseDown() {
+		//Application.LoadLevel("BaconShooter");
+		Debug.Log("Clicked!");
+		if (rigidbody2D.isKinematic){
+			rigidbody2D.isKinematic = false;
+		}
+		clicked = true;
+	}
+}
